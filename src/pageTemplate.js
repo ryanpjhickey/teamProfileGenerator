95fb32f607a8f834f60ac0c37861f8114014ae10
Ryan <ryanpjhickey@gmail.com>
2022-10-14 21:28:09 -0700

    setup templates for each role

--- src/pageTemplate.js
+++ src/pageTemplate.js
@@ -18,3 +18,45 @@ const generateManager = manager => {
     </div>
     `
 }
+
+const generateIntern = intern => {
+    return `
+    <div class='card employee-card'>
+    <div class='card-header'>
+        <h2 class='card-title'>${intern.getName()}</h2>
+        <h3 class='card-title'><i class='fas fa-mug-hot mr-2'></i>${intern.getRole()}</h3>
+        </div>
+        <div class='card-body'>
+            <ul class='list-group'>
+            <li class='list-group-item'>ID: ${intern.getId()}
+            </li>
+            <li class='list-group-item'>E-mail: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a>
+            </li>
+            <li class='list-group-item'>Studying at: ${intern.getSchool()}
+            </li>
+            </ul>
+        </div>
+    </div>
+    `
+}
+
+const generateEngineer = engineer => {
+    return `
+    <div class='card employee-card'>
+    <div class='card-header'>
+        <h2 class='card-title'>${engineer.getName()}</h2>
+        <h3 class='card-title'><i class='fas fa-mug-hot mr-2'></i>${engineer.getRole()}</h3>
+        </div>
+        <div class='card-body'>
+            <ul class='list-group'>
+            <li class='list-group-item'>ID: ${engineer.getId()}
+            </li>
+            <li class='list-group-item'>E-mail: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a>
+            </li>
+            <li class='list-group-item'>GitHub: ${engineer.getGithub()}
+            </li>
+            </ul>
+        </div>
+    </div>
+    `
+}
